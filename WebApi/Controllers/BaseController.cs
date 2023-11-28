308fec1a7c01b82fee065557f069f26aa80391c0
Fuat Aydın <GitHub_Email_Adresi>
2023-11-28 10:49:33 +0300

    Add project files.

---
+++ WebApi/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        private IMediator? _mediator;
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>(); //mediator var ise onu al yok ise injectionlara bak ve onu ekle.
+    }
+}
